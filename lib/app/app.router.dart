6a354c310126758c11d061adbd0b26ed943bdf8b
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-05-11 01:51:39 -0300

    preparing playground

--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -5,12 +5,15 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter/material.dart' as _i5;
+import 'package:flutter/material.dart' as _i7;
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i6;
+import 'package:stacked_services/stacked_services.dart' as _i8;
 import 'package:test_node_flutter/ui/views/home/home_view.dart' as _i2;
+import 'package:test_node_flutter/ui/views/playground/playground_view.dart'
+    as _i6;
 import 'package:test_node_flutter/ui/views/startup/startup_view.dart' as _i3;
+import 'package:test_node_flutter/ui/views/todo/todo_view.dart' as _i5;
 import 'package:test_node_flutter/ui/views/transactions/transactions_view.dart'
     as _i4;
 
@@ -21,10 +24,16 @@ class Routes {
 
   static const transactionsView = '/transactions-view';
 
+  static const todoView = '/todo-view';
+
+  static const playgroundView = '/playground-view';
+
   static const all = <String>{
     homeView,
     startupView,
     transactionsView,
+    todoView,
+    playgroundView,
   };
 }
 
@@ -42,24 +51,44 @@ class StackedRouter extends _i1.RouterBase {
       Routes.transactionsView,
       page: _i4.TransactionsView,
     ),
+    _i1.RouteDef(
+      Routes.todoView,
+      page: _i5.TodoView,
+    ),
+    _i1.RouteDef(
+      Routes.playgroundView,
+      page: _i6.PlaygroundView,
+    ),
   ];
 
   final _pagesMap = <Type, _i1.StackedRouteFactory>{
     _i2.HomeView: (data) {
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i7.MaterialPageRoute<dynamic>(
         builder: (context) => const _i2.HomeView(),
         settings: data,
       );
     },
     _i3.StartupView: (data) {
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i7.MaterialPageRoute<dynamic>(
         builder: (context) => const _i3.StartupView(),
         settings: data,
       );
     },
     _i4.TransactionsView: (data) {
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i7.MaterialPageRoute<dynamic>(
         builder: (context) => const _i4.TransactionsView(),
+        settings: data,
+      );
+    },
+    _i5.TodoView: (data) {
+      return _i7.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i5.TodoView(),
+        settings: data,
+      );
+    },
+    _i6.PlaygroundView: (data) {
+      return _i7.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i6.PlaygroundView(),
         settings: data,
       );
     },
@@ -72,7 +101,7 @@ class StackedRouter extends _i1.RouterBase {
   Map<Type, _i1.StackedRouteFactory> get pagesMap => _pagesMap;
 }
 
-extension NavigatorStateExtension on _i6.NavigationService {
+extension NavigatorStateExtension on _i8.NavigationService {
   Future<dynamic> navigateToHomeView([
     int? routerId,
     bool preventDuplicates = true,
@@ -115,6 +144,34 @@ extension NavigatorStateExtension on _i6.NavigationService {
         transition: transition);
   }
 
+  Future<dynamic> navigateToTodoView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.todoView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> navigateToPlaygroundView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.playgroundView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
   Future<dynamic> replaceWithHomeView([
     int? routerId,
     bool preventDuplicates = true,
@@ -151,6 +208,34 @@ extension NavigatorStateExtension on _i6.NavigationService {
         transition,
   ]) async {
     return replaceWith<dynamic>(Routes.transactionsView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithTodoView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.todoView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithPlaygroundView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.playgroundView,
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
